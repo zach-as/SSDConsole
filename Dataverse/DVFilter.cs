323210cd4b40e19f0ef93a76b7d7a5a5aea46879
Zachary Ander-Sturtz <zach.andersturtz@sturtzsolutions.com>
2025-06-13 13:00:45 -0700

    Updated DVFilter to make several functions internal instead of private.

--- Dataverse/DVFilter.cs
+++ Dataverse/DVFilter.cs
@@ -66,51 +66,6 @@ namespace SSDConsole.Dataverse
                 // filter = Name && (ID || (Ln1 && (Ln2 || Sprs)))
                 filter.AddFilter(filter_name);
                 filter.AddFilter(filter_addr);
-                
-                /*
-                filter_addr.AddCondition(Attribute.AddressID, ConditionOperator.Equal, clinic.Location.AddressID);
-
-                var filter_addr_lns = new FilterExpression(LogicalOperator.And);
-                filter_addr_lns.AddCondition(Attribute.AddressLine1, ConditionOperator.Equal, clinic.Location.AddressLine1);
-
-                var filter_addr_ln2 = new FilterExpression(LogicalOperator.Or);
-
-                var filter_addr_ln2_1 = new FilterExpression(LogicalOperator.Or); // Check if the line 2 matches
-                if (string.IsNullOrEmpty(clinic.Location.AddressLine2))
-                {
-                    filter_addr_ln2_1.AddCondition(Attribute.AddressLine2, ConditionOperator.Equal, "");
-                    filter_addr_ln2_1.AddCondition(Attribute.AddressLine2, ConditionOperator.Null);
-                } else
-                {
-                    filter_addr_ln2_1.AddCondition(Attribute.AddressLine2, ConditionOperator.Equal, clinic.Location.AddressLine2);
-                }
-
-                
-                var filter_addr_ln2_2 = new FilterExpression(LogicalOperator.Or); // Check if the line 2 does not match but the address is still the same thanks to supression
-                filter_addr_ln2_2.AddCondition(Attribute.Line2Suppressed, ConditionOperator.Equal, true); // Is the ln 2 supressed in the DV record? If so, the local line 2 does not matter
-                var filter_addr_ln2_2_1 = new FilterExpression(LogicalOperator.And);
-                filter_addr_ln2_2_1.AddCondition(Attribute.Line2Suppressed, ConditionOperator.Equal, false); // Is the ln 2 not supressed in the DV record?
-
-                // if the ln 2 is not supressed in the dv record, check to make sure the local line 2 matches the DV line 2
-                if (!string.IsNullOrEmpty(clinic.Location.AddressLine2))
-                {
-                    filter_addr_ln2_2_1.AddCondition(Attribute.AddressLine2, ConditionOperator.Equal, clinic.Location.AddressLine2); // is the line 2 the same?
-                } else if (!clinic.Location.Line2Suppressed) // if the local clinic's line 2 is supressed, we skip this comparison
-                {
-                    var filter_addr_ln2_2_1_1 = new FilterExpression(LogicalOperator.Or);
-                    filter_addr_ln2_2_1_1.AddCondition(Attribute.AddressLine2, ConditionOperator.Null);
-                    filter_addr_ln2_2_1_1.AddCondition(Attribute.AddressLine2, ConditionOperator.Equal, "");
-                    filter_addr_ln2_2_1.AddFilter(filter_addr_ln2_2_1_1);
-                }
-                filter_addr_ln2_2.AddFilter(filter_addr_ln2_2_1); // If the remote record is not supressed, does the line 2 match?
-                
-                // Check if the addresses are the same
-                filter_addr_ln2.AddFilter(filter_addr_ln2_1); // are addr ln 2 the same?
-                filter_addr_ln2.AddFilter(filter_addr_ln2_2); // are addr ln 2 not the same but the address is still the same thanks to supression?
-                filter_addr_lns.AddFilter(filter_addr_ln2); // is addr ln 1 the same and the are the other conditions met?
-                filter_addr.AddFilter(filter_addr_lns); // is addr id the same or are the address lines the same?
-                filter.AddFilter(filter_addr); // are the addresses the same?
-                filter.AddFilter(filter_name); // are the names the same?*/
             }
             else if (a is Clinician)
             {
@@ -127,15 +82,15 @@ namespace SSDConsole.Dataverse
             return filter;
         }
 
-        private static void AddEquals(this FilterExpression f, Attribute a, object value = null)
+        internal static void AddEquals(this FilterExpression f, Attribute a, object value)
             => f.AddCondition(a, ConditionOperator.Equal, value);
-        private static void AddNotEquals(this FilterExpression f, Attribute a, object value = null)
+        internal static void AddNotEquals(this FilterExpression f, Attribute a, object value)
             => f.AddCondition(a, ConditionOperator.NotEqual, value);
-        private static void AddNull(this FilterExpression f, Attribute a)
+        internal static void AddNull(this FilterExpression f, Attribute a)
             => f.AddCondition(a, ConditionOperator.Null);
-        private static void AddNotNull(this FilterExpression f, Attribute a)
+        internal static void AddNotNull(this FilterExpression f, Attribute a)
             => f.AddCondition(a, ConditionOperator.NotNull);
-        private static void AddCondition(this FilterExpression f, Attribute a, ConditionOperator op, object? value = null)
+        internal static void AddCondition(this FilterExpression f, Attribute a, ConditionOperator op, object? value = null)
         {
             switch (op)
             {
