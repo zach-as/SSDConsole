5c405e7f1536fb5e8ae25fed0d4fa5eabd47da47
Zachary Ander-Sturtz <zach.andersturtz@sturtzsolutions.com>
2025-06-27 12:55:21 -0700

    Moved SOptionSet functions into partial class of SConnector for better organization of DVConnector related functions.

--- LibDV/Connector/SConnector_OptionSet.cs
+++ LibDV/Connector/SConnector_OptionSet.cs
@@ -1,7 +1,8 @@
-﻿using LibUtil.UtilDisplay;
+﻿using LibDV.OptionSet;
+using LibUtil.UtilDisplay;
+using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Messages;
 using Microsoft.Xrm.Sdk.Metadata;
-using Microsoft.Xrm.Sdk.Organization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,71 +13,60 @@ namespace LibDV.Connector
 {
     internal static partial class SConnector
     {
+        internal static COptionSet GetOptionSetData(string logicalName)
+            => new COptionSet(GetOptionSetMeta(logicalName));
+
         // This adds the provided labels to the option set with the provided logical name
-        internal static void AddOptionSetData(string logicalname, IEnumerable<string> labels)
+        internal static void AddOptionSetData(string logicalName, List<string> labels)
         {
-            SDisplay.Print($"Adding {labels.Count()} labels to the option set: {logicalname}.");
+            var existingOptionSet = GetOptionSetData(logicalName);
+            var labelsToAdd = existingOptionSet.MissingLabels(labels);
 
-            var meta = GetOptionSetMeta(logicalname);
-            var existingLabels = meta.Options.Select(o => o.Label.UserLocalizedLabel.Label);
-            var labelsToAdd = labels.Except(existingLabels); // only add labels that do not already exist
+            if (labelsToAdd.Count() == 0)
+            {
+                SDisplay.Print($"No labels to add for option set: {logicalName}. Skipping.");
+                return;
+            }
 
-            int added = 0;
-            int total = labelsToAdd.Count();
+            SDisplay.Print($"Updating the option set: {logicalName}.");
+            SDisplay.Print($"Adding {labelsToAdd.Count()} labels.");
+
+            SDisplay.StartProgressBar("Labels added", labelsToAdd.Count());
 
             // Relate the new labels
             foreach (string labelToAdd in labelsToAdd)
             {
-                Console.CursorLeft = 0;
-                Console.Write($"Labels added: {added} / {total}.");
-
-                added++;
-
-                InsertOptionSetLabel(logicalname, labelToAdd);
+                SDisplay.UpdateProgressBar();
+                InsertOptionSetLabel(logicalName, labelToAdd);
             }
 
-            Console.WriteLine();
-
-            Console.WriteLine("Label addition complete.");
+            SDisplay.StopProgressBar();
+            SDisplay.Success("Successfully added {labelsToAdd.Count()} labels.");
         }
 
-        private static InsertOptionValueResponse? InsertOptionSetLabel(string logicalname, string label)
-        {
-            // Create a request.
-            InsertOptionValueRequest request = new()
-            {
-                OptionSetName = logicalname,
-                Label = new Label(label, 1033),
-                SolutionUniqueName = SOLUTION,
-            };
+        private static void InsertOptionSetLabel(string logicalName, string label)
+            => Service().Execute(
+                new InsertOptionValueRequest()
+                {
+                    OptionSetName = logicalName,
+                    Label = new Label(label, 1033),
+                    SolutionUniqueName = SOLUTION,
+                });
 
-            // Execute the request
-            return (InsertOptionValueResponse)Service().Execute(request);
-        }
-
-        private static OptionSetMetadata GetOptionSetMeta(string logicalname)
+        private static OptionSetMetadata GetOptionSetMeta(string logicalName)
         {
             var request = new RetrieveOptionSetRequest
             {
-                Name = logicalname
+                Name = logicalName
             };
             var response = (RetrieveOptionSetResponse)Service().Execute(request);
 
             if (response is null)
             {
-                throw new Exception($"Failed to retrieve optionset: {logicalname}");
+                throw new Exception($"Failed to retrieve optionset: {logicalName}");
             }
 
             return (OptionSetMetadata)response.OptionSetMetadata;
         }
-
-        internal static List<COptionSetEntry> GetOptionSetData(string logicalname)
-            => GetOptionSetMeta(logicalname)
-                .Options.Select(
-                    o => new COptionSetEntry(
-                    o.Label.UserLocalizedLabel.Label,
-                    o.Value ?? 0))
-                .ToList();
     }
-    
 }
