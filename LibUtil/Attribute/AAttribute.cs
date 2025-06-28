e21ca6bce029f7ad690ad43320d9673fa14269a1
Zachary Ander-Sturtz <zach.andersturtz@sturtzsolutions.com>
2025-06-28 09:37:29 -0700

    Added AAttributeOverrideValue for overriding the value when retrieving specific fields by attribute tags.

--- LibUtil/Attribute/AAttribute.cs
+++ LibUtil/Attribute/AAttribute.cs
@@ -22,4 +22,38 @@ namespace LibUtil.UtilAttribute
         public override string ToString() => logicalName;
         public string LogicalName() => logicalName;
     }
+
+    // An attribute for overridiing the value of an attribute in a class.
+    // This is most useful if the existing value in the relevant field should not be directly used.
+    public class AAttributeOverrideValueAttribute : System.Attribute
+    {
+        private object? value;
+        private Func<object?>? valueFunc;
+
+        public AAttributeOverrideValueAttribute(object? value)
+        {
+            this.value = value;
+        }
+        public AAttributeOverrideValueAttribute(Func<object?> valueFunc)
+        {
+            this.valueFunc = valueFunc;
+        }
+
+        public object? Value()
+        {
+            if (valueFunc != null)
+                return valueFunc();
+            return value;
+        }
+    }
+
+    public static class SAttributeOverride
+    {
+        public static object? Value(this AAttributeOverrideValueAttribute attr)
+        {
+            if (attr == null)
+                return null;
+            return attr.Value();
+        }
+    }
 }
