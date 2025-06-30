942af305382d60e6f86b37fad8b77ba1163abc6e
Zachary Ander-Sturtz <zach.andersturtz@sturtzsolutions.com>
2025-06-30 15:59:34 -0700

    Main Program WIP

--- Console/Program.cs
+++ Console/Program.cs
@@ -5,6 +5,7 @@ using LibDV.DVEntity;
 using LibUtil.UtilGlobal;
 using LibCMS.Data.Associable;
 using LibUtil.UtilDisplay;
+using LibDV.EntityType;
 
 namespace Console
 {
@@ -29,13 +30,46 @@ namespace Console
             var clinicEntities = new CEntitySet(clinics.AsParent());
             var medicalGroupEntities = new CEntitySet(medicalGroups.AsParent());
 
-            // Organize the entity data into a singular object
+            // Retrieve existing entity data from DV
+            var DVClinicians = SConnectorDV.FetchEntities(EEntityType.Clinician);
+            var DVClinics = SConnectorDV.FetchEntities(EEntityType.Clinic);
+            var DVMedicalGroups = SConnectorDV.FetchEntities(EEntityType.MedicalGroup);
+
+            // Identify the new entitites from CMS which must be pushed to DV
+            var newClinicians = clinicianEntities.Excluding(DVClinicians);
+            var newClinics = clinicEntities.Excluding(DVClinics);
+            var newMedicalGroups = medicalGroupEntities.Excluding(DVMedicalGroups);
+
+            // Organize the new entity data into a singular object for pushing to DV
+            var allNewEntities = new CEntitySuperSet();
+            allNewEntities.AddSet(newClinicians);
+            allNewEntities.AddSet(newClinics);
+            allNewEntities.AddSet(newMedicalGroups);
+
+            // Push the new entity data to DV
+            // This will also generate IDs for each entity and update the information in allNewEntities accordingly
+            allNewEntities = SConnectorDV.PushEntityCreate(allNewEntities);
+
+            // Identify the entities from CMS that already exist in DV and should be updated
+            var existingClinicians = clinicianEntities.Overlapping(DVClinicians);
+            var existingClinics = clinicEntities.Overlapping(DVClinics);
+            var existingMedicalGroups = medicalGroupEntities.Overlapping(DVMedicalGroups);
+
+            // Organize the entity data to be updated to DV into a singular object
+            var allExistingEntities = new CEntitySuperSet();
+            allExistingEntities.AddSet(existingClinicians);
+            allExistingEntities.AddSet(existingClinics);
+            allExistingEntities.AddSet(existingMedicalGroups);
+
+            // Push the updated entity data to DV
+            SConnectorDV.PushEntityUpdate(allExistingEntities);
+
+            // Merge all existing and new entities into one set
             var allEntities = new CEntitySuperSet();
-            allEntities.AddSet(clinicianEntities);
-            allEntities.AddSet(clinicEntities);
-            allEntities.AddSet(medicalGroupEntities);
+            allEntities.AddSet(allNewEntities);
+            allEntities.AddSet(allExistingEntities);
 
-
+            var relationships = S
         }
     }
 }
