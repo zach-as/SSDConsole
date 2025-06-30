28ba91aaa160cc1a1d98537186fd04c25d95abb7
Zachary Ander-Sturtz <zach.andersturtz@sturtzsolutions.com>
2025-06-30 10:42:47 -0700

    Removed throw not implemented exception in CRecord. Not needed anymore.

--- LibCMS/Record/CRecord.cs
+++ LibCMS/Record/CRecord.cs
@@ -129,7 +129,7 @@ namespace LibCMS.Record
             // Create a temp clinic for comparison purposes
             CClinic compareClinic = new CClinic(item);
             clinic = Clinics().FirstOrDefault(c => c.Equals(compareClinic)); // use the custom comparison operators
-            throw new NotImplementedException(); // TODO: c.Equals() no longer works because I got rid of the override. fix this shit
+
             if (clinic is null)
             {
                 // Save the temp clinic as a permanent clinic
