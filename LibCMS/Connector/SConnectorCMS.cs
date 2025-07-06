edc7e64922f36bc4c8208938e3c73e761c9ead12
Zachary Ander-Sturtz <zach.andersturtz@sturtzsolutions.com>
2025-07-06 09:19:06 -0700

    Added parallelization to CMS queries.

--- LibCMS/Connector/SConnectorCMS.cs
+++ LibCMS/Connector/SConnectorCMS.cs
@@ -4,6 +4,7 @@ using LibCMS.Params;
 using LibCMS.Record;
 using LibCMS.Http;
 using LibCMS.Data.Associable;
+using System.Threading.Tasks;
 
 namespace LibCMS.Connector
 {
@@ -50,22 +51,43 @@ namespace LibCMS.Connector
 
             CParameters parameters = new CParameters();
             parameters.Limit = 30;
-            int? recordTotal = 30;
-            int recordsRecorded = 0;
 
-            do
+            if (records is null) records = new CRecordOutput();
+            var requests = await BuildRequests(parameters);
+
+            SDisplay.Print($"Built {requests.Count} requests to pull records from CMS. Processing.");
+
+            var responses = await Task.WhenAll(requests); // send all requests in parallel
+
+            SDisplay.Print($"Received {responses.Length} responses from CMS. Processing records.");
+            SDisplay.StartProgressBar("Processing record sets", responses.Length);
+
+            foreach (var response in responses)
             {
-                var message = await Send(new CHttpRequest(parameters)); // query CMS
+                var recordResponse = await ProcessRecordResponse(response); // convert the response to usable data in a struct
+                records.AddRecordInput(recordResponse); // Convert the data to most usable form (clinicians, clinics, medical groups, etc)
+                SDisplay.UpdateProgressBar();
+            }
 
-                var response = await CRecordResponse.BuildFromHttpResponse(message); // convert text to usable data
+            SDisplay.StopProgressBar();
+            SDisplay.Print("Records from CMS queried and processed.");
 
-                if (response is null) throw new Exception("Failed to build record response from text.");
+            /*do
+            {
+                var message = Send(new CHttpRequest(parameters)); // query CMS
+                var response = CRecordResponse.BuildFromHttpResponse(message); // convert text to usable data
+
+                records.AddRecordInput(initResponse); // Convert the response to most usable form (clinicians, clinics, orgs, etc)
+
+                var tasks = new List<Task<HttpResponseMessage>>(); // list of send tasks to run in parallel
+                var responses = new List<CRecordResponse>(); // list of responses to convert into usable forms
+
+
+                if (response is null) throw new Exception("Failed to build record httpResponse from text.");
                 if (recordTotal is null || recordTotal == 0) recordTotal = response.RecordCountDB; // note the total # of records
 
                 if (!SDisplay.InProgress()) SDisplay.StartProgressBar("CMS records pulled and formatted", recordTotal);
 
-                if(records is null) records = new CRecordOutput();
-                records.AddRecordInput(response); // Convert the response to most usable form (clinicians, clinics, orgs, etc)
 
                 int recordsPulled = response.Records().Count();
                 
@@ -81,10 +103,72 @@ namespace LibCMS.Connector
             } while (recordTotal is not null
                     && recordsRecorded < recordTotal);
 
-            SDisplay.StopProgressBar();
-            SDisplay.Print("Records from CMS pulled and updated.");
+            SDisplay.StopProgressBar();*/
 
         }
+
+        private static async Task<HttpResponseMessage> SendRequest(CParameters parameters)
+        {
+            var request = await Send(new CHttpRequest(parameters));
+            
+            if (request is null) throw new Exception("Failed to send request to CMS.");
+            if (!request.IsSuccessStatusCode)
+            {
+                throw new Exception($"Failed to send request to CMS. Status code: {request.StatusCode}, Reason: {request.ReasonPhrase}");
+            }
+
+            if (SDisplay.InProgress())
+                SDisplay.UpdateProgressBar(parameters.Limit ?? 2000);
+
+            return request;
+        }
+        private static async Task<CRecordResponse> ProcessRecordResponse(Task<HttpResponseMessage> response)
+            => await ProcessRecordResponse(await response);
+        private static async Task<CRecordResponse> ProcessRecordResponse(HttpResponseMessage response)
+        {
+            var recordResponse = await BuildRecordResponse(response);
+            if (recordResponse is null) throw new Exception("Failed to build record CRecordResponse from HttpResponse.");
+            return recordResponse;
+        }
+        private static Task<CRecordResponse?> BuildRecordResponse(HttpResponseMessage response)
+            => CRecordResponse.BuildFromHttpResponse(response);
+
+        // This builds a list of requests to send to the CMS based on the parameters provided.
+        // Note that none of the requests are sent until the list is returned and Task.Run() or await is used.
+        private static async Task<List<Task<HttpResponseMessage>>> BuildRequests(CParameters parameters)
+        {
+            var httpResponse = SendRequest(parameters); // query CMS to retrieve certain information such as records total
+            var recordResponse = await ProcessRecordResponse(httpResponse); // convert the response to usable data
+
+            // TODO: Reset this back to RecordCountDB when done testing
+            var recordTotal = 50; //recordResponse.RecordCountDB;
+            var recordsPulled = recordResponse.Records().Count();
+            var queryLimit = parameters.Limit ?? 2000; // default to 2000 if not specified
+
+            SDisplay.Print("Querying CMS records.");
+            SDisplay.StartProgressBar("CMS records queried", recordTotal);
+
+            int remainingRecords = recordTotal - recordsPulled; // how many records are left to query?
+            var requests = new List<Task<HttpResponseMessage>>();
+            while (remainingRecords > 0)
+            {
+                var newLimit = Math.Min(queryLimit, remainingRecords);
+                // Create a new request with the updated offset and limit
+                var newParameters = new CParameters()
+                {
+                    Offset = parameters.Offset + recordsPulled,
+                    Limit = newLimit,
+                };
+                requests.Add(SendRequest(newParameters));
+                recordsPulled += newLimit;
+                remainingRecords -= newLimit;
+            }
+
+            SDisplay.StopProgressBar();
+            
+            return requests;
+        }
+
         private static bool ShouldUpdateRecords()
         {
             if (records is null) return true;
